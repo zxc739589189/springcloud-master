9928b677a9aa4510618ca85ce8bb1d3346a973c4
zxc739589189 <739589189@qq.com>
2019-05-29 23:49:05 +0800

    ribbon

---
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
@@ -0,0 +1,16 @@
+package com.funtl.hello.spring.cloud.web.admin.ribbon;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class WebAdminRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminRibbonApplication.class,args);
+    }
+
+}
