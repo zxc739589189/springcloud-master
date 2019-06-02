e2eacedccd916cf5f9cfceb846c5e25b96831768
zxc739589189 <739589189@qq.com>
2019-06-03 00:00:19 +0800

    6.3提交 熔断

--- hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/service/AdminService.java
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/service/AdminService.java
@@ -1,14 +1,15 @@
 package com.funtl.hello.spring.cloud.web.admin.feign.service;
 
 
+import com.funtl.hello.spring.cloud.web.admin.feign.service.hystrix.AdminServiceHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "hello-spring-cloud-service-admin")
+@FeignClient(value = "hello-spring-cloud-service-admin",fallback = AdminServiceHystrix.class)
 public interface AdminService {
 
     @RequestMapping(value = "hi", method = RequestMethod.GET)
-    public String sayHi(@RequestParam(value = "name") String message);
+     String sayHi(@RequestParam(value = "name") String message);
 }
